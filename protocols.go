146b61421d7cf410fb2d6b1b8c88de869e63eea6
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-11-18 22:25:12 -0800

    protocols

---
+++ protocols.go
@@ -0,0 +1,15 @@
+package nodeinfo
+
+const (
+	ProtocolActivityPub = "activitypub"
+	ProtocolBuddyCloud  = "buddycloud"
+	ProtocolDFRN        = "dfrn"
+	ProtocolDiaspora    = "diaspora"
+	ProtocolLiberTree   = "libertree"
+	ProtocolNostr       = "nostr"
+	ProtocolOStatus     = "ostatus"
+	ProtocolPumpIO      = "pumpio"
+	ProtocolTent        = "tent"
+	ProtocolXMPP        = "xmpp"
+	ProtocolZot         = "zot"
+)
