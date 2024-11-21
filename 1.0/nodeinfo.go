e015ed199e7a4cb6e859ccd0ebaa04cc94d11dae
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-11-21 08:06:56 -0800

    shared & internal

--- 1.0/nodeinfo.go
+++ 1.0/nodeinfo.go
@@ -1,7 +1,7 @@
 package nodeinfo
 
 import (
-	"encoding/json"
+	"github.com/reiver/go-nodeinfo/internal"
 )
 
 type NodeInfo struct {
@@ -14,114 +14,7 @@ type NodeInfo struct {
 }
 
 func (receiver NodeInfo) MarshalJSON() ([]byte, error) {
-	var buffer [256]byte
-	var bytes []byte = buffer[0:0]
+	const version string = "1.0"
 
-	bytes = append(bytes, '{')
-
-	{
-		const field string = `"version":"1.0"`
-
-		bytes = append(bytes, field...)
-	}
-
-	bytes = append(bytes, ',')
-
-	{
-		const prefix string = `"software":`
-
-		bytes = append(bytes, prefix...)
-
-		value, err := json.Marshal(receiver.Software)
-		if nil != err {
-			return nil, err
-		}
-
-		bytes = append(bytes, value...)
-	}
-
-	bytes = append(bytes, ',')
-
-	{
-		const prefix string = `"protocols":`
-
-		bytes = append(bytes, prefix...)
-
-		value, err := json.Marshal(receiver.Protocols)
-		if nil != err {
-			return nil, err
-		}
-
-		bytes = append(bytes, value...)
-	}
-
-	bytes = append(bytes, ',')
-
-	{
-		const prefix string = `"services":`
-
-		bytes = append(bytes, prefix...)
-
-		value, err := json.Marshal(receiver.Services)
-		if nil != err {
-			return nil, err
-		}
-
-		bytes = append(bytes, value...)
-	}
-
-	bytes = append(bytes, ',')
-
-	{
-		const prefix string = `"openRegistrations":`
-		var data bool = receiver.OpenRegistrations
-
-		bytes = append(bytes, prefix...)
-
-		if data {
-			bytes = append(bytes, `true`...)
-		} else {
-			bytes = append(bytes, `false`...)
-		}
-
-	}
-
-	bytes = append(bytes, ',')
-
-	{
-		const prefix string = `"usage":`
-
-		bytes = append(bytes, prefix...)
-
-		value, err := json.Marshal(receiver.Usage)
-		if nil != err {
-			return nil, err
-		}
-
-		bytes = append(bytes, value...)
-	}
-
-	bytes = append(bytes, ',')
-
-	{
-		const prefix string = `"metadata":`
-		var data map[string]any = receiver.MetaData
-
-		bytes = append(bytes, prefix...)
-
-		if len(data) <= 0 {
-			bytes = append(bytes, `{}`...)
-		} else {
-			value, err := json.Marshal(data)
-			if nil != err {
-				return nil, err
-			}
-
-			bytes = append(bytes, value...)
-		}
-	}
-
-	bytes = append(bytes, '}')
-
-	return bytes, nil
+	return internal.NodeInfoMarshalJSON(version, receiver.Software,  receiver.Protocols,  receiver.Services,  receiver.OpenRegistrations,  receiver.Usage,  receiver.MetaData)
 }
---
+++ shared/usage.go
@@ -0,0 +1,88 @@
+package shared
+
+import (
+	"github.com/reiver/go-erorr"
+	"github.com/reiver/go-jsonint"
+	"github.com/reiver/go-opt"
+)
+
+type Usage struct {
+	Users Users
+	LocalPosts opt.Optional[string]
+	LocalComments opt.Optional[string]
+}
+
+func (receiver Usage) MarshalJSON() ([]byte, error) {
+	var buffer [256]byte
+	var bytes []byte = buffer[0:0]
+
+	bytes = append(bytes, '{')
+
+	{
+		const prefix string = `"users":`
+		bytes = append(bytes, prefix...)
+
+		p, err := receiver.Users.MarshalJSON()
+		if nil != err {
+			return nil, err
+		}
+
+		bytes = append(bytes, p...)
+	}
+
+	{
+		const prefix string = `"localPosts":`
+
+		err := func() error {
+			value, found :=  receiver.LocalPosts.Get()
+			if !found {
+				return nil
+			}
+
+			if !jsonint.IsNumericString(value) {
+				return erorr.Errorf("nodeinfo: usage.localPosts value %q is not an integer", value)
+			}
+
+			bytes = append(bytes, ',')
+
+			bytes = append(bytes, prefix...)
+			bytes = append(bytes, value...)
+
+			return nil
+		}()
+		if nil != err {
+			return nil, err
+		}
+
+	}
+
+	{
+		const prefix string = `"localComments":`
+
+		err := func() error {
+			value, found :=  receiver.LocalComments.Get()
+			if !found {
+				return nil
+			}
+
+			if !jsonint.IsNumericString(value) {
+				return erorr.Errorf("nodeinfo: usage.localComments value %q is not an integer", value)
+			}
+
+			bytes = append(bytes, ',')
+
+			bytes = append(bytes, prefix...)
+			bytes = append(bytes, value...)
+
+			return nil
+		}()
+		if nil != err {
+			return nil, err
+		}
+
+	}
+
+	bytes = append(bytes, '}')
+
+	return bytes, nil
+}
