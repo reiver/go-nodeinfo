09a4e7a2c2bc967f4d3c9b3c0f298a5d9e1f3fb6
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-11-24 18:09:29 -0800

    nodeinfo 1.1 protocols

---
+++ 1.1/protocolnames.go
@@ -0,0 +1,19 @@
+package nodeinfo
+
+import (
+	"github.com/reiver/go-nodeinfo/shared"
+)
+
+const (
+	ProtocolBuddyCloud  = shared.ProtocolBuddyCloud  // "buddycloud"
+	ProtocolDiaspora    = shared.ProtocolDiaspora    // "diaspora"
+	ProtocolFriendica   = shared.ProtocolFriendica   // "friendica"
+	ProtocolGNUSocial   = shared.ProtocolGNUSocial   // "gnusocial"
+	ProtocolLiberTree   = shared.ProtocolLiberTree   // "libertree"
+	ProtocolMediaGoblin = shared.ProtocolMediaGoblin // "mediagoblin"
+	ProtocolPumpIO      = shared.ProtocolPumpIO      // "pumpio"
+	ProtocolRedMatrix   = shared.ProtocolRedMatrix   // "redmatrix"
+	ProtocolSMTP        = shared.ProtocolSMTP        // "smtp"
+	ProtocolTent        = shared.ProtocolTent        // "tent"
+	ProtocolZot         = shared.ProtocolZot         // "zot"
+)
