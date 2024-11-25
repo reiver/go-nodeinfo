08e00cf4bcdc4a8ee2e37dc50af927b89a8ae8c9
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-11-24 20:40:37 -0800

    shared inbound services

--- shared/inboundservices.go
+++ shared/inboundservices.go
@@ -1,14 +1,14 @@
 package shared
 
 const (
-	ServiceInboundAppNet    = "appnet"
-	ServiceInboundAtom1     = "atom1.0"
-	ServiceInboundGNUSocial = "gnusocial"
-	ServiceInboundIMAP      = "imap"
-	ServiceInboundPnut      = "pnut"
-	ServiceInboundPOP3      = "pop3"
-	ServiceInboundPumpIO    = "pumpio"
-	ServiceInboundRSS2      = "rss2.0"
-	ServiceInboundTwitter   = "twitter"
-	ServiceInboundZot       = "zot"
+	ServiceInboundAppNet    = "appnet"    // app.net
+	ServiceInboundAtom1     = "atom1.0"   // Atom Syndication Format version 1.
+	ServiceInboundGNUSocial = "gnusocial" // GNU social
+	ServiceInboundIMAP      = "imap"      // Internet Message Access Protocol (IMAP) — defined in IETF RFC 9051. (An e-mail receiving protocol.)
+	ServiceInboundPnut      = "pnut"      // pnut.io
+	ServiceInboundPOP3      = "pop3"      // Post Office Protocol version 3 (POP3). (An e-mail receiving protocol.)
+	ServiceInboundPumpIO    = "pumpio"    // pump.io
+	ServiceInboundRSS2      = "rss2.0"    // Really Simple Syndication (RSS) version 2
+	ServiceInboundTwitter   = "twitter"   // Twitter. Later renamed to 'X'.
+	ServiceInboundZot       = "zot"       // Zot!
 )
