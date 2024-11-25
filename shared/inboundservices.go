5f64cede58da98d168585b6f014ba6c0614071eb
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-11-24 18:57:29 -0800

    shared inbound services

--- shared/inboundservices.go
+++ shared/inboundservices.go
@@ -2,7 +2,13 @@ package shared
 
 const (
 	ServiceInboundAppNet    = "appnet"
+	ServiceInboundAtom1     = "atom1.0"
 	ServiceInboundGNUSocial = "gnusocial"
+	ServiceInboundIMAP      = "imap"
+	ServiceInboundPnut      = "pnut"
+	ServiceInboundPOP3      = "pop3"
 	ServiceInboundPumpIO    = "pumpio"
+	ServiceInboundRSS2      = "rss2.0"
+	ServiceInboundTwitter   = "twitter"
 	ServiceInboundZot       = "zot"
 )
