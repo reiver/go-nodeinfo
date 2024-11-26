c41f88665b6ab2eafd7157d871f2987a20b63d01
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-11-25 22:20:53 -0800

    http.Handler

--- internal/jsonservehttp.go
+++ internal/jsonservehttp.go
@@ -1,7 +1,10 @@
 package internal
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -38,7 +41,52 @@ func JSONServeHTTP(responseWriter http.ResponseWriter, request *http.Request, js
 		}
 	}
 
+	var digest [sha256.Size]byte
 	{
+		digest = sha256.Sum256(bytes)
+	}
+
+	var cacheDigest string
+	{
+		cacheDigest = fmt.Sprintf("sha-256=:%s:", base64.StdEncoding.EncodeToString(digest[:]))
+	}
+
+	var eTag string
+	{
+		var format string = fmt.Sprintf("sha256=0x%%0%dX", sha256.Size*2)
+		eTag = fmt.Sprintf(format, digest[:])
+	}
+
+	{
+		var header http.Header = responseWriter.Header()
+		if nil == header {
+			var code int = http.StatusInternalServerError
+			var text string = http.StatusText(code)
+
+			http.Error(responseWriter, text, code)
+			return
+		}
+
+		header.Add("Cache-Control", "max-age=907")
+		header.Add("Content-Digest", cacheDigest)
+		header.Add("Content-Type", "application/json; charset=utf-8")
+		header.Add("ETag", `"`+eTag+`"`)
+	}
+
+	{
+		var header http.Header = request.Header
+		if nil != header {
+			var ifNoneMatch string = header.Get("If-None-Match")
+			if eTag == ifNoneMatch {
+				responseWriter.WriteHeader(http.StatusNotModified)
+				return
+			}
+		}
+	}
+
+	{
+		responseWriter.WriteHeader(http.StatusOK)
+
 		responseWriter.Write(bytes)
 	}
 }
