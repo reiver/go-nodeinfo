9deee3d438702ffae1852578bde0d14d399c983c
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-11-25 19:37:13 -0800

    nodeinfo 2.2

---
+++ 2.2/outboundservices.go
@@ -0,0 +1,36 @@
+package nodeinfo
+
+import (
+	"github.com/reiver/go-nodeinfo/shared"
+)
+
+const (
+	ServiceOutboundAtom1         = shared.ServiceOutboundAtom1         // "atom1.0"
+	ServiceOutboundBlogger       = shared.ServiceOutboundBlogger       // "blogger"
+	ServiceOutboundBuddycloud    = shared.ServiceOutboundBuddycloud    // "buddycloud"
+	ServiceOutboundDiaspora      = shared.ServiceOutboundDiaspora      // "diaspora"
+	ServiceOutboundDreamwidth    = shared.ServiceOutboundDreamwidth    // "dreamwidth"
+	ServiceOutboundDrupal        = shared.ServiceOutboundDrupal        // "drupal"
+	ServiceOutboundFacebook      = shared.ServiceOutboundFacebook      // "facebook"
+	ServiceOutboundFriendica     = shared.ServiceOutboundFriendica     // "friendica"
+	ServiceOutboundGNUSocial     = shared.ServiceOutboundGNUSocial     // "gnusocial"
+	ServiceOutboundGoogle        = shared.ServiceOutboundGoogle        // "google"
+	ServiceOutboundInsaneJournal = shared.ServiceOutboundInsaneJournal // "insanejournal"
+	ServiceOutboundLibertree     = shared.ServiceOutboundLibertree     // "libertree"
+	ServiceOutboundLinkedIn      = shared.ServiceOutboundLinkedIn      // "linkedin"
+	ServiceOutboundLiveJournal   = shared.ServiceOutboundLiveJournal   // "livejournal"
+	ServiceOutboundMediaGoblin   = shared.ServiceOutboundMediaGoblin   // "mediagoblin"
+	ServiceOutboundMySpace       = shared.ServiceOutboundMySpace       // "myspace"
+	ServiceOutboundPinterest     = shared.ServiceOutboundPinterest     // "pinterest"
+	ServiceOutboundPnut          = shared.ServiceOutboundPnut          // "pnut"
+	ServiceOutboundPosterous     = shared.ServiceOutboundPosterous     // "posterous"
+	ServiceOutboundPumpIO        = shared.ServiceOutboundPumpIO        // "pumpio"
+	ServiceOutboundRedMatrix     = shared.ServiceOutboundRedMatrix     // "redmatrix"
+	ServiceOutboundRSS2          = shared.ServiceOutboundRSS2          // "rss2.0"
+	ServiceOutboundSMTP          = shared.ServiceOutboundSMTP          // "smtp"
+	ServiceOutboundTent          = shared.ServiceOutboundTent          // "tent"
+	ServiceOutboundTumblr        = shared.ServiceOutboundTumblr        // "tumblr"
+	ServiceOutboundTwitter       = shared.ServiceOutboundTwitter       // "twitter"
+	ServiceOutboundWordPress     = shared.ServiceOutboundWordPress     // "wordpress"
+	ServiceOutboundXMPP          = shared.ServiceOutboundXMPP          // "xmpp"
+)
